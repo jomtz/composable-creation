c9ae594752c7d7dd5573e1c697893f0842b8993c
jomtz <46319076+jomtz@users.noreply.github.com>
2023-07-21 13:17:22 -0400

    Change theming and style

--- app/src/main/java/com/jomartinez/composablecreation/MainActivity.kt
+++ app/src/main/java/com/jomartinez/composablecreation/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.jomartinez.composablecreation
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -57,7 +58,6 @@ fun OnboardingScreen(
     onContinueClicked: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-
     Column(
         modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -108,10 +108,10 @@ private fun Greeting(name: String) {
         Row(modifier = Modifier.padding(24.dp)) {
             Column(modifier = Modifier
                 .weight(1f)
-                .padding(bottom = extraPadding)
+                .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(text = name, style = MaterialTheme.typography.headlineMedium)
             }
             ElevatedButton(
                 onClick = { expanded.value = !expanded.value }
@@ -122,6 +122,12 @@ private fun Greeting(name: String) {
     }
 }
 
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Dark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
--- app/src/main/java/com/jomartinez/composablecreation/ui/theme/Theme.kt
+++ app/src/main/java/com/jomartinez/composablecreation/ui/theme/Theme.kt
@@ -10,19 +10,28 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
+    surface = Blue,
+    onSurface = Navy,
+    primary = Navy,
+    onPrimary = Chartreuse,
     secondary = PurpleGrey80,
     tertiary = Pink80
 )
 
+
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    surface = Blue,
+    onSurface = White,
+    primary = LightBlue,
+    onPrimary = Navy,
     secondary = PurpleGrey40,
     tertiary = Pink40
 
